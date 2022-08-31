f2e91a559ffae0d0fe3b6d18b9065bf598b7e891
SuperKXT <saadkhankxt@hotmail.com>
2022-08-31 16:39:47 +0500

    Setup mongoose logging

--- src/helpers/connect-db/connect-db.helpers.ts
+++ src/helpers/connect-db/connect-db.helpers.ts
@@ -1,14 +1,24 @@
 import mongoose from 'mongoose';
+import util from 'util';
 
 import config from '~/config';
 
 import logger from '~/helpers/logger';
 
+const messageMapper = (message: string) => {
+	return util
+		.inspect(message, false, 10, true)
+		.replace(/\n/g, '').replace(/\s{2,}/g, ' ');
+};
+
 const connectDb = async () => {
 	try {
-		const connection = await mongoose.connect(config.dbUri);
+		await mongoose.connect(config.dbUri);
+		mongoose.set('debug', (collectionName, methodName, ...methodArgs) => {
+			logger.info(`\x1B[0;36mMongoose:\x1B[0m ${collectionName}.${methodName}` + `(${methodArgs.map(messageMapper).join(', ')})`);
+		});
+
 		logger.info('Connected to DB');
-		return connection;
 	}
 	catch (error: any) {
 		throw new Error(`Error Connecting to DB: ${error.message}`);
