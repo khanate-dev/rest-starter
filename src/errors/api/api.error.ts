ef9ba0879b13aa528335fc6dd9ead048bf9fb889
SuperKXT <saadkhankxt@hotmail.com>
2022-09-01 16:31:40 +0500

    Improve and extend ApiError

--- src/errors/api/api.error.ts
+++ src/errors/api/api.error.ts
@@ -1,23 +1,29 @@
-// // type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-// // type OneToFour = 1 | 2 | 3 | 4;
-// // type HttpCode = `${OneToFour}${Digit}${Digit}`;
+import { Status } from '~/types';
 
 const defaultMessage = 'Something went wrong';
 
-const messages: Record<string, string> = {
+const messages: Partial<Record<Status, string>> = {
+	401: 'Not logged in',
 	403: 'You do not have permission to perform this action',
 	404: 'Resource not found',
 };
 
 class ApiError extends Error {
-	code: number;
-	constructor(code: number, message?: string, options?: ErrorOptions) {
+
+	code: Status;
+
+	constructor(
+		code: Status,
+		message?: string,
+		options?: ErrorOptions
+	) {
 		super(
 			message ?? messages[code] ?? defaultMessage,
 			options
 		);
 		this.code = code;
 	}
+
 }
 
 export default ApiError;
