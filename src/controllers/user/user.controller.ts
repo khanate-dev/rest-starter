7350714a85d8b83cd588b1840b8201c4c2c0ba9b
SuperKXT <saadkhankxt@hotmail.com>
2022-09-01 16:30:00 +0500

    Overhaul user controller

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,21 +1,18 @@
-import { RequestHandler } from 'express';
-
-import logger from '~/helpers/logger';
+import { UserWithoutPassword } from '~/models';
 
 import { CreateUserInput } from '~/schemas/user';
 
 import { createUser } from '~/services/user';
 
-export const createUserHandler: RequestHandler<any, any, CreateUserInput['body']> = async (
-	request,
-	response
-) => {
-	try {
-		const user = await createUser(request.body);
-		return response.send(user);
-	}
-	catch (error: any) {
-		logger.error(error);
-		return response.status(409).send(error.message ?? error);
-	}
+import { PublicHandler, Status } from '~/types';
+
+export const createUserHandler: PublicHandler<
+	CreateUserInput,
+	UserWithoutPassword
+> = async (request) => {
+	const user = await createUser(request.body);
+	return {
+		status: Status.CREATED,
+		json: user,
+	};
 };
