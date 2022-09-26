c31f0c27b16dee424bc5d4a7c79fe199cf5a39b5
mbm_1607 <muhammadkhan1607@gmail.com>
2022-09-26 13:11:03 +0500

    Add cors to app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import pinoMiddleWare from 'express-pino-logger';
 import helmet from 'helmet';
+import cors from 'cors';
 
 import config from '~/config';
 
@@ -12,22 +13,39 @@ import registerRoutes from '~/register-routes';
 
 const app = express();
 
+const corsOptions: cors.CorsOptions = {
+  allowedHeaders: [
+    'x-refresh',
+    'Content-Type',
+    'Authorization',
+  ],
+  exposedHeaders: [
+    'x-access-token',
+  ],
+};
+
+// TODO Modify origin to correct production origin
+if (config.env === 'production') {
+  corsOptions.origin = 'example.com';
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 
 if (config.env === 'production') {
-	app.use(pinoMiddleWare({ logger }));
+  app.use(pinoMiddleWare({ logger }));
 }
 
 const server = app.listen(config.port, async () => {
-	logger.info(`App is running at http://localhost:${config.port}`);
-	connectDb();
-	registerRoutes(app);
+  logger.info(`App is running at http://localhost:${config.port}`);
+  connectDb();
+  registerRoutes(app);
 });
 
 server.on('error', (error) => {
-	logger.fatal(error);
+  logger.fatal(error);
 });
 
 export default app;
