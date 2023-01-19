d1cfe6956b101fdea739b316b135d507c9696f77
mbm_1607 <muhammadkhan1607@gmail.com>
2023-01-19 21:54:28 +0500

    :loud_sound: Update depracated pinoMiddleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import pinoMiddleWare from 'express-pino-logger';
+import pinoHttp from 'pino-http';
 import helmet from 'helmet';
 import cors from 'cors';
 
@@ -13,38 +13,35 @@ import registerRoutes from '~/register-routes';
 const app = express();
 
 const corsOptions: cors.CorsOptions = {
-	allowedHeaders: [
-		'x-refresh',
-		'Content-Type',
-		'Authorization',
-	],
-	exposedHeaders: [
-		'x-access-token',
-	],
+  allowedHeaders: [
+    'x-refresh',
+    'Content-Type',
+    'Authorization',
+  ],
+  exposedHeaders: [
+    'x-access-token',
+  ],
 };
 
 // TODO Modify origin to correct production origin
 if (config.env === 'production') {
-	corsOptions.origin = 'example.com';
+  corsOptions.origin = 'example.com';
 }
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-
-if (config.env === 'production') {
-	app.use(pinoMiddleWare({ logger }));
-}
+app.use(pinoHttp({ logger }));
 
 const server = app.listen(config.port, async () => {
-	logger.info(`App is running at http://localhost:${config.port}`);
-	connectDb();
-	registerRoutes(app);
+  logger.info(`App is running at http://localhost:${config.port}`);
+  connectDb();
+  registerRoutes(app);
 });
 
 server.on('error', (error) => {
-	logger.fatal(error);
+  logger.fatal(error);
 });
 
 export default app;
