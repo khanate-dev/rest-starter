aae63ffc2cce0926fbb75ce13e69dfbad2c0caff
SuperKXT <superkxt@outlook.com>
2023-02-28 21:12:42 +0500

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,81 +1,114 @@
 // @ts-check
 
 /** @type {import('eslint').Linter.Config} */
-module.exports = {
+const config = {
 	env: {
 		es2021: true,
-		jest: true,
 		node: true,
+		jest: true,
 	},
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-	],
+	extends: ['eslint:recommended', 'prettier'],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 'latest',
 		sourceType: 'module',
 	},
-	plugins: [
-		'@typescript-eslint',
-		'unused-imports',
-	],
+	plugins: ['import', 'unused-imports'],
 	rules: {
-		indent: ['off', 'tab'],
-		'linebreak-style': ['error', 'unix'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'always'],
-		'comma-dangle': ['error', {
-			arrays: 'always-multiline',
-			objects: 'always-multiline',
-			imports: 'always-multiline',
-			exports: 'always-multiline',
-			functions: 'never',
-		}],
-		'no-console': ['error', {
-			allow: ['warn', 'error', 'info'],
-		}],
-		'no-multiple-empty-lines': ['error', {
-			max: 1,
-			maxEOF: 0,
-			maxBOF: 0,
-		}],
-		'eol-last': ['error', 'always'],
-		'quote-props': ['error', 'as-needed'],
-		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/no-inferrable-types': 'off',
-		'@typescript-eslint/no-unnecessary-type-constraint': 'off',
-		'@typescript-eslint/member-delimiter-style': ['error', {
-			multiline: {
-				delimiter: 'comma',
-				requireLast: true,
+		radix: ['warn', 'as-needed'],
+		'import/extensions': 'warn',
+		'import/order': [
+			'error',
+			{
+				'newlines-between': 'always',
+				groups: [
+					'builtin',
+					'external',
+					'internal',
+					'parent',
+					['sibling', 'index', 'object'],
+					'type',
+				],
+				pathGroups: [
+					{
+						pattern: '~/helpers/**',
+						group: 'internal',
+					},
+				],
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true,
+				},
 			},
-			singleline: {
-				delimiter: 'comma',
-				requireLast: true,
+		],
+		'import/no-duplicates': 'warn',
+		'import/no-default-export': 'error',
+		'no-console': [
+			'error',
+			{
+				allow: ['warn', 'error', 'info'],
 			},
-		}],
-		'no-duplicate-imports': ['warn', {
-			includeExports: true,
-		}],
+		],
 		'no-unused-vars': 'off',
 		'unused-imports/no-unused-imports': 'warn',
 		'unused-imports/no-unused-vars': [
 			'warn',
-			{ vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+			{
+				vars: 'all',
+				varsIgnorePattern: '^_',
+				args: 'after-used',
+				argsIgnorePattern: '^_',
+			},
 		],
-		'@typescript-eslint/no-unused-vars': 'off',
 		'object-shorthand': ['error', 'always'],
+		'no-constant-condition': [
+			'error',
+			{
+				checkLoops: false,
+			},
+		],
 	},
 	overrides: [
 		{
-			files: ['*.test.ts'],
-			extends: ['plugin:jest/all'],
-			plugins: ['jest'],
-			rules: {
-				'jest/prefer-expect-assertions': 'off',
-				'jest/require-top-level-describe': 'off',
+			files: ['**/*.ts', '**/*.tsx'],
+			parserOptions: {
+				project: './tsconfig.json',
 			},
+			extends: ['plugin:@typescript-eslint/recommended'],
+			plugins: ['@typescript-eslint'],
+			rules: {
+				'@typescript-eslint/no-extra-semi': 'off',
+				'@typescript-eslint/no-explicit-any': 'off',
+				'@typescript-eslint/no-inferrable-types': 'off',
+				'@typescript-eslint/no-unnecessary-type-constraint': 'off',
+				'@typescript-eslint/prefer-nullish-coalescing': 'off',
+				'@typescript-eslint/no-unused-vars': 'off',
+				'@typescript-eslint/no-unnecessary-condition': 'error',
+			},
+			overrides: [
+				{
+					files: ['*.test.ts'],
+					extends: ['plugin:jest/all'],
+					plugins: ['jest'],
+					parserOptions: {
+						project: ['./tsconfig.json'],
+					},
+					rules: {
+						'jest/prefer-expect-assertions': 'off',
+						'jest/require-top-level-describe': 'off',
+					},
+				},
+			],
 		},
 	],
+	settings: {
+		'import/resolver': {
+			typescript: {},
+		},
+		jest: {
+			version: 29,
+		},
+	},
 };
+
+module.exports = config;
