b3426a7b8546304d13ad31e97bab867b6adda629
SuperKXT <superkxt@outlook.com>
2023-03-07 23:44:40 +0500

    Add prisma-client.ts

---
+++ src/prisma-client.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from '@prisma/client';
+
+import { CONFIG } from '~/config';
+import { LOGGER } from '~/helpers/logger';
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const prisma = new PrismaClient({
+	errorFormat: CONFIG.env === 'development' ? 'pretty' : undefined,
+	log: [
+		{
+			emit: 'event',
+			level: 'query',
+		},
+		{
+			emit: 'event',
+			level: 'error',
+		},
+		{
+			emit: 'event',
+			level: 'info',
+		},
+		{
+			emit: 'event',
+			level: 'warn',
+		},
+	],
+});
+
+prisma.$on('query', LOGGER.info);
+prisma.$on('info', LOGGER.info);
+prisma.$on('error', LOGGER.warn);
+prisma.$on('warn', LOGGER.error);
