f4421784443cbd988b6744842bbae3fc87be4c0e
SuperKXT <superkxt@outlook.com>
2023-07-19 18:14:04 +0500

    Update `prettier.config.cjs`

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+const config = {
+	useTabs: true,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	singleAttributePerLine: true,
+};
+
+module.exports = config;
