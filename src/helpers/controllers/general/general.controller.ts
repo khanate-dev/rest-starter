a08791c7ce5a142155605dcb57c0c6e0e4201d29
SuperKXT <superkxt@outlook.com>
2023-07-20 18:16:50 +0500

    Remove `controllers`

--- src/helpers/controllers/general/general.controller.ts
+++
@@ -1,8 +0,0 @@
-import type { EchoSchema } from '~/schemas/general';
-import type { UnAuthenticatedHandler } from '~/types';
-
-export const echoHandler: UnAuthenticatedHandler<EchoSchema> = async () =>
-	Promise.resolve({
-		message: 'The server can be reached',
-		success: true,
-	});
