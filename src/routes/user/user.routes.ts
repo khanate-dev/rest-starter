bf5cf6119b1198b11f1c084fae0562717dd7fe9a
SuperKXT <superkxt@outlook.com>
2023-07-20 18:18:30 +0500

    Overhaul `routes/user`

--- src/routes/user/user.routes.ts
+++ src/routes/user/user.routes.ts
@@ -1,37 +1,55 @@
-import {
-	createUserHandler,
-	getUserHandler,
-	getUsersHandler,
-} from '~/helpers/controllers/user';
-import {
-	CREATE_USER_SCHEMA,
-	GET_USERS_SCHEMA,
-	GET_USER_SCHEMA,
-} from '~/schemas/user';
+import { z } from 'zod';
 
-import type { Route } from '~/types';
+import { ApiError } from '~/errors';
+import { STATUS } from '~/helpers/http';
+import { createContract, createRoutes } from '~/helpers/route';
+import { userSansMetaSchema, userSansPasswordSchema } from '~/schemas/user';
+import { createUser, findUserById, findUsers } from '~/services/user';
 
-export const userRoutes: Route[] = [
-	{
-		handler: createUserHandler,
+export const userContract = createContract({
+	get: {
+		method: 'get',
+		path: '/',
+		auth: true,
+		availableTo: 'admin',
+		response: z.array(userSansPasswordSchema),
+	},
+	getOne: {
+		method: 'get',
+		path: '/:id',
+		auth: false,
+		availableTo: 'admin',
+		params: { id: 'string' },
+		response: userSansPasswordSchema,
+	},
+	post: {
 		method: 'post',
 		path: '/',
-		schema: CREATE_USER_SCHEMA,
+		body: userSansMetaSchema
+			.extend({
+				passwordConfirmation: z.string(),
+			})
+			.refine((data) => data.password === data.passwordConfirmation, {
+				message: 'Passwords do not match',
+				path: ['passwordConfirmation'],
+			}),
+		response: userSansPasswordSchema,
 	},
-	{
-		availableTo: 'admin',
-		handler: getUsersHandler,
-		isAuthenticated: true,
-		method: 'get',
-		path: '/',
-		schema: GET_USERS_SCHEMA,
+});
+
+export const userRoutes = createRoutes(userContract, {
+	get: async () => {
+		const users = await findUsers();
+		return users;
 	},
-	{
-		availableTo: 'admin',
-		handler: getUserHandler,
-		isAuthenticated: true,
-		method: 'get',
-		path: '/:_id',
-		schema: GET_USER_SCHEMA,
+	getOne: async ({ params }) => {
+		const user = await findUserById(params.id);
+		if (!user)
+			throw new ApiError(STATUS.notFound, 'the requested user was not found');
+		return user;
 	},
-];
+	post: async (request) => {
+		const user = await createUser(request.body);
+		return { json: user, status: STATUS.created };
+	},
+});
