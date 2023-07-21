5cfaedb8c680a0906dabdfdbf13e46305dd2bf9b
SuperKXT <superkxt@outlook.com>
2023-07-21 15:03:41 +0500

    Update `helpers/schema`

--- src/helpers/schema/schema.helpers.ts
+++ src/helpers/schema/schema.helpers.ts
@@ -1,11 +1,26 @@
 import { z } from 'zod';
 
-export const mongoIdSchema = z.string();
+import { regex } from '~/constants';
 
-export const mongoMetaSchema = z.strictObject({
-	createdAt: z.date(),
-	id: mongoIdSchema,
-	updatedAt: z.date(),
+import type { Utils } from '~/types/utils';
+
+export const portSchema = z.coerce
+	.number()
+	.int()
+	.positive()
+	.min(1024)
+	.max(65535);
+
+export const dbIdSchema = z.string().regex(regex.mongoId);
+
+export type ZodDbId = typeof dbIdSchema;
+
+export type DbId = z.infer<ZodDbId>;
+
+export const dbMetaSchema = z.strictObject({
+	id: dbIdSchema,
+	created_at: z.date(),
+	updated_at: z.date(),
 });
 
 export const createModelSchema = <
@@ -17,9 +32,29 @@ export const createModelSchema = <
 	const sansMetaModelSchema = z.strictObject(schema);
 
 	const modelSchema = z.strictObject({
-		...mongoMetaSchema.shape,
+		...dbMetaSchema.shape,
 		...schema,
 	});
 
 	return [sansMetaModelSchema, modelSchema] as const;
+};
+
+export const zodAllOrNone = <T extends Record<string, z.Schema>>(
+	shape: T,
+): z.Schema<
+	Utils.allOrNone<
+		Utils.makeUndefinedOptional<{ [k in keyof T]: T[k]['_output'] }>
+	>
+> => {
+	return z.strictObject(shape).or(
+		z.object(
+			Object.keys(shape).reduce(
+				(acc, key) => {
+					acc[key] = z.undefined();
+					return acc;
+				},
+				{} as Record<string, z.ZodUndefined>,
+			),
+		),
+	);
 };
