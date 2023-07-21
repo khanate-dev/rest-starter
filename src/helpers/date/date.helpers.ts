5dcf76d3f15fc1f45a619c27f7fd25046a1455f6
SuperKXT <superkxt@outlook.com>
2023-07-21 15:02:36 +0500

    Add `helpers/date`

---
+++ src/helpers/date/date.helpers.ts
@@ -0,0 +1,43 @@
+import { default as dayjs } from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+
+dayjs.extend(utc);
+
+export const dayjsUtc = dayjs;
+
+export const isDate = (value: unknown): value is string | Date => {
+	if (
+		typeof value !== 'string' &&
+		typeof value !== 'number' &&
+		!(value instanceof Date)
+	)
+		return false;
+
+	const date = value instanceof Date ? value : new Date(value);
+	return !isNaN(date.getTime());
+};
+
+export const getDateOrNull = (value: unknown): null | Date => {
+	if (
+		typeof value !== 'string' &&
+		typeof value !== 'number' &&
+		!(value instanceof Date)
+	)
+		return null;
+
+	const date = value instanceof Date ? value : new Date(value);
+	if (isNaN(date.getTime())) return null;
+
+	return date;
+};
+
+export const compareDate = (
+	first: string | Date,
+	second: string | Date,
+): number => new Date(first).getTime() - new Date(second).getTime();
+
+export const dayjsFormatPatterns = {
+	date: 'YYYY-MM-DD',
+	time: 'h:mm:ss A',
+	datetime: 'YYYY-MM-DD h:mm A',
+};
