2639dc650b5fd1a5602b4de09936752d7332ae5f
SuperKXT <superkxt@outlook.com>
2023-07-25 18:57:23 +0500

    Move express type overrides to separate file

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,12 +1,2 @@
-import type { JwtPayload } from '~/helpers/auth';
-
 type Obj = Record<string, unknown>;
 type EmptyObj = Record<string, never>;
-
-declare global {
-	namespace Express {
-		interface Locals {
-			user: JwtPayload;
-		}
-	}
-}
