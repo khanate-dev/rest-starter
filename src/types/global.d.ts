9fad21cd01ab5be63054196daffb498816d3165f
SuperKXT <superkxt@outlook.com>
2023-07-25 18:46:46 +0500

    Add `user` to `Locals` express global type

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,2 +1,12 @@
+import type { JwtPayload } from '~/helpers/auth';
+
 type Obj = Record<string, unknown>;
 type EmptyObj = Record<string, never>;
+
+declare global {
+	namespace Express {
+		interface Locals {
+			user: JwtPayload;
+		}
+	}
+}
